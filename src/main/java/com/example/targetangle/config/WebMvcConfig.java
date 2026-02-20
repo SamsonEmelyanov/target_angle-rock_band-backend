06b4278108970312251bd3a1c0604d3bc2004d2a
samntn <samsom.emelyanov@gmail.com>
2026-02-20 10:24:20 +0400

    migrate to java 21 and spring boot 2.7.18

--- src/main/java/com/example/targetangle/config/WebMvcConfig.java
+++ src/main/java/com/example/targetangle/config/WebMvcConfig.java
@@ -2,20 +2,28 @@ package com.example.targetangle.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.pattern.PathPatternParser;
 
+@EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
     @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        PathPatternParser parser = new PathPatternParser();
+        parser.setMatchOptionalTrailingSeparator(true);
+        configurer.setPatternParser(parser);
+    }
+
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true)
+                .allowedOriginPatterns("http://*", "https://*")
         .maxAge(MAX_AGE_SECS);
     }
 }
