b33c71b7dd1fdc46e220f5c433a1eb25069d316a
samntn <samsom.emelyanov@gmail.com>
2026-02-21 23:58:52 +0400

    tests fix

--- src/test/java/com/example/targetangle/TargetAngleApplicationTests.java
+++ src/test/java/com/example/targetangle/TargetAngleApplicationTests.java
@@ -2,9 +2,8 @@ package com.example.targetangle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+//@SpringBootTest
 public class TargetAngleApplicationTests {
 
 	@Test
